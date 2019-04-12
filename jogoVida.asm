6bce2058a397ca0b225af634b711c55d0d2fc14e
unknown <johpetsc@gmail.com>
2019-04-12 17:39:31 -0300

    Lendo da matriz

--- jogoVida.asm
+++ jogoVida.asm
@@ -1,46 +1,43 @@
 .data
-	endereco: .word 0x2000
+	endereco: .word 0x1000
 	branco: .word 0x20B2AA
 	amarelo: .word 0x006400
-	x: .word 3 #coordenada x-1
-	y: .word 7 #coordenada y-1
+	x: .word 1 #coordenada x-1
+	y: .word 1 #coordenada y-1
 	xmul: .word 4 # multiplicador para a linha
 	ymul: .word 64 # multiplicador para a coluna
-	.align 2
 	matrizA: .space 0x400
-	.align 2
 	matrizB: .space 0x400
 	newline: .asciz "\n"
 .text
 	
 
 matA:	
-	addi a0, zero, 1
 	addi a1, zero, 0
-	la t1, matrizA
+	la t3, matrizA
 	addi t5, zero, 256
 	
 loopA:
 	beq a1, t5, matB
 	addi a0, zero, 0
-	sw a0, 0(t1)
+	sw a0, 0(t3)
 	li a7, 1
 	ecall
 	la a0, newline
 	li a7, 4
 	ecall
 	addi a1, a1, 1
-	addi t1, t1, 4
+	addi t3, t3, 4
 	j loopA
+
 matB:
-	addi a0, zero, 1
 	addi a1, zero, 0
 	la t2, matrizB
 	addi t5, zero, 256
 	
 loopB:
 	beq a1, t5, init
-	addi a0, zero, 1
+	addi a0, zero, 0
 	sw a0, 0(t2)
 	li a7, 1
 	ecall
@@ -50,12 +47,12 @@ loopB:
 	addi a1, a1, 1
 	addi t2, t2, 4
 	j loopB
-	
+
 init:
 	lw s0, endereco # carrega o endereco inicial
 	lw s1, branco 
 	lw s2, amarelo
-	li s3, 0x2400 # carrega o endereco final (16x16x4=1024, hexadecimal = 400)
+	li s3, 0x1400 # carrega o endereco final (16x16x4=1024, hexadecimal = 400)
 	lw s4, x
 	lw s5, xmul
 	mul s4, s4, s5
@@ -63,28 +60,53 @@ init:
 	lw s6, ymul
 	mul s5, s5, s6
 	add s4, s4, s5 #endereco da coordenada
+
 	
 tela:	#preenche a tela de branco
-	beq s0, s3, pixel
+	beq s0, s3, estado
 	sw s1, 0(s0)
 	addi s0, s0, 4
 	j tela
-	
-pixel: #adiciona um pixel a coordenada
-	addi s0, s0, -1024
-	add s0, s0, s4
-	sw s2, 0(s0)
-	sw s2, 64(s0)
-	sw s2, 68(s0)
-	
+
 sleep:
 	li a0, 1000
 	li a7, 32
 	ecall
-	
+
+estado:
+	addi s0, s0, -1024
+	la t3, matrizA
+	add t3, t3, s4
+	addi a0, zero, 1
+	sw a0, 0(t3)
+	sw a0, 64(t3)
+	sw a0, 68(t3)
+	sub t3, t3, s4
+	addi t4, zero, 1
+	addi t6, zero, 0
+	addi t5, zero, 0
+	addi t0, zero, 256
+verifica:
+	beq t4, t6, pixel
+	beq t5, t0, fim
+	lw t6, 0(t3)
+	addi t3, t3, 4
+	addi t5, t5, 1
+	j verifica
+
+pixel:	
+	addi t5, t5, -1
+	lw t6, xmul
+	mul t6, t6, t5
+	add s0, s0, t6
+	sw s2, 0(s0)
+	sub s0, s0, t6
+	addi t5, t5, 1
+	addi t6, zero, 0
+	j verifica
 fim:	
 	#la t2, matrizB
-	#lw a0, 1020(t2)
+	#lw a0, 460(t2)
 	#li a7, 1
 	#ecall
 	
