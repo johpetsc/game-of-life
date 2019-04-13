9b2e948d860ea5877d2eade34e1b18ac0089538f
unknown <johpetsc@gmail.com>
2019-04-12 23:03:48 -0300

    Funcionando sem entrada de teclado

--- jogoVida.asm
+++ jogoVida.asm
@@ -1,22 +1,22 @@
 .data
-	endereco: .word 0x1000
-	branco: .word 0x20B2AA
+	endereco: .word 0x1000 #endereco inicial bitmap
+	fundo: .word 0x20B2AA #cor de fundo
 	amarelo: .word 0x006400
-	x: .word 1 #coordenada x-1
-	y: .word 1 #coordenada y-1
+	x: .word 5 #coordenada x-1
+	y: .word 5 #coordenada y-1
 	xmul: .word 4 # multiplicador para a linha
 	ymul: .word 64 # multiplicador para a coluna
-	matrizA: .space 0x400
-	matrizB: .space 0x400
+	matrizA: .space 0x400 #armazena espaco para a matriz A
+	matrizB: .space 0x400 
 	newline: .asciz "\n"
 .text
 	
-matA:	
+matA:	#carrega o endereco da matriz A
 	addi a1, zero, 0
 	la t3, matrizA
 	addi t5, zero, 256
 	
-loopA:
+loopA:	#popula a matriz A com 0
 	beq a1, t5, matB
 	addi a0, zero, 0
 	sw a0, 0(t3)
@@ -24,12 +24,12 @@ loopA:
 	addi t3, t3, 4
 	j loopA
 
-matB:
+matB:	#carrega o endereco da matriz B
 	addi a1, zero, 0
 	la t2, matrizB
 	addi t5, zero, 256
 	
-loopB:
+loopB: #popula a matriz B com 0
 	beq a1, t5, init
 	addi a0, zero, 0
 	sw a0, 0(t2)
@@ -39,33 +39,55 @@ loopB:
 
 init:
 	lw s0, endereco # carrega o endereco inicial
-	lw s1, branco 
+	lw s1, fundo 
 	lw s2, amarelo
 	li s3, 0x1400 # carrega o endereco final (16x16x4=1024, hexadecimal = 400)
-	lw s4, x
-	lw s5, xmul
-	mul s4, s4, s5
-	lw s5, y
+	lw s4, x # calcula o endereco so primeiro pixel
+	lw t3, xmul
+	mul s4, s4, t3
+	lw t3, y
 	lw s6, ymul
-	mul s5, s5, s6
-	add s4, s4, s5 #endereco da coordenada
+	mul t3, t3, s6
+	add s4, s4, t3 #endereco da coordenada
 
-	
-tela:	#preenche a tela de branco
-	beq s0, s3, estado
-	sw s1, 0(s0)
-	addi s0, s0, 4
-	j tela
-
-estado:
-	addi s0, s0, -1024
+estado:	#constroi o estado inicial da matriz
 	la t3, matrizA
 	add t3, t3, s4
 	addi a0, zero, 1
 	sw a0, 0(t3)
-	sw a0, 64(t3)
+	sw a0, 4(t3)
 	sw a0, 68(t3)
+	sw a0, -60(t3)
+	sw a0, -56(t3)
 	sub t3, t3, s4
+
+estadoA:
+	li a0, 1000
+	li a7, 32
+	ecall
+	addi s5, zero, 0
+	j tela
+
+estadoB:
+	li a0, 1000
+	li a7, 32
+	ecall
+	addi s5, zero, 1
+
+tela:	#preenche a tela de fundo
+	beq s0, s3, sleep
+	sw s1, 0(s0)
+	addi s0, s0, 4
+	j tela
+
+sleep:	# prepara o proximo estado
+	beq s5, zero, A
+	la t3, matrizB
+	j B
+A:
+	la t3, matrizA
+B:
+	addi s0, s0, -1024 #volta para o endereco inicial do bitmap
 	addi t4, zero, 1
 	addi t6, zero, 0
 	addi t5, zero, 0
@@ -73,17 +95,12 @@ estado:
 	addi s10, zero, 1 #x
 	addi s9, zero, 1 #y
 	addi s8, zero, 16
-
-sleep:
-	li a0, 1000
-	li a7, 32
-	ecall
 		
-percorre:
-	beq t4, t6, pixel
-	beq t5, a1, fim
-	beq s9, s8, soma
-	lw t6, 0(t3)
+percorre: # percorre a matriz
+	beq t4, t6, pixel # preenche o pixel da bacteria
+	beq t5, a1, fim # termina a matriz
+	beq s9, s8, soma # calcula a posicao x, y da matriz
+	lw t6, 0(t3) 
 	j verifica
 cont:
 	addi t3, t3, 4
@@ -91,81 +108,86 @@ cont:
 	addi s9, s9, 1
 	j percorre
 
-soma:
+soma:	#s10 = posicao x / s9 = posicao y
 	addi s10, s10, 1
 	addi s9, zero, 1
 	j cont
 	
-verifica:
+verifica: #verifica o proximo estado do pixel
+	beq s5, zero, B1
+	la t2, matrizA
+	j A1
+B1:
 	la t2, matrizB
+A1:
 	mul s7, t5, t0
 	add t2, t2, s7
 	sw t6, 0(t2)
 	addi s11, zero, 0
-	bgt s10, t4, verifica1
-	bgt s9, t4, verifica4
-	j verifica6
+	bgt s10, t4, verifica1 # se estiver na primeira linha, nao testa a linha de cima
+	bgt s9, t4, verifica4 # se estiver na primeira coluna, nao testa a coluna da esquerda
+	j verifica6 # se estiver na primeira coluna e linha, nao testa cima e esquerda
 	
-verifica1:
+verifica1:# x-1
 	lw a0, -64(t3)
 	add s11, s11, a0
 	beq s9, t4, verifica3
 
-verifica2:
+verifica2:# x-1, y-1
 	lw a0, -68(t3)
 	add s11, s11, a0
 	beq s9, s8, verifica4
 
-verifica3:
+verifica3:# x-1, y+1
 	lw a0, -60(t3)
 	add s11, s11, a0
 	beq s9, t4, verifica6
 	
-verifica4:
+verifica4:# y-1
 	lw a0, -4(t3)
 	add s11, s11, a0
 	beq s10, s8, verifica8
 
-verifica5:
+verifica5:# x+1, y-1
 	lw a0, 60(t3)
 	add s11, s11, a0
 
-verifica6:
+verifica6:# x+1
 	lw a0, 64(t3)
 	add s11, s11, a0
 	beq s9, s8, teste
 	
-verifica7:
+verifica7:# x+1, y+1
 	lw a0, 68(t3)
 	add s11, s11, a0
 	
-verifica8:
+verifica8:# y+1
 	lw a0, 4(t3)
 	add s11, s11, a0
 	j teste
 
-teste:	
+teste:	# testa o que deve acontecer com o pixel
 	addi a2, zero, 0
-	beq s11, a1, morte
+	beq s11, a1, morte # 0 vizinhos, estado 0
 	addi a2, a2, 1
-	beq s11, a2, morte
+	beq s11, a2, morte # 1 vizinhos, estado 0
 	addi a2, a2, 1
-	beq s11, a2, cont
+	beq s11, a2, cont # 2 vizinhos, estado nao muda(0 ou 1)
 	addi a2, a2, 1
-	beq s11, a2, vida
-	bgt s11, a2, morte
+	beq s11, a2, vida # 3 vizinhos, estado 1
+	bgt s11, a2, morte # 3+ vizinhos, estado 0
 	
-morte:	
+morte:	#estado 0
 	addi a2, zero, 0
 	sw a2, 0(t2)
 	j cont
 
-vida:	
+vida:	#estado 1
 	addi a2, zero, 1
 	sw a2, 0(t2)
 	j cont
 	
-pixel:	
+pixel:	#preenche o pixel
 	addi t5, t5, -1
 	lw t6, xmul
 	mul t6, t6, t5
@@ -176,8 +198,6 @@ pixel:
 	addi t6, zero, 0
 	j percorre
 fim:	
-	#la t2, matrizB
-	#lw a0, 460(t2)
-	#li a7, 1
-	#ecall
+	beq s5, zero, estadoB
+	j estadoA
 	
