287c20a8bf5c48b27e9303b476f761e105afdc65
unknown <johpetsc@gmail.com>
2019-04-12 21:34:27 -0300

    Atualização de matrizes

--- jogoVida.asm
+++ jogoVida.asm
@@ -11,7 +11,6 @@
 	newline: .asciz "\n"
 .text
 	
-
 matA:	
 	addi a1, zero, 0
 	la t3, matrizA
@@ -21,11 +20,6 @@ loopA:
 	beq a1, t5, matB
 	addi a0, zero, 0
 	sw a0, 0(t3)
-	li a7, 1
-	ecall
-	la a0, newline
-	li a7, 4
-	ecall
 	addi a1, a1, 1
 	addi t3, t3, 4
 	j loopA
@@ -39,11 +33,6 @@ loopB:
 	beq a1, t5, init
 	addi a0, zero, 0
 	sw a0, 0(t2)
-	li a7, 1
-	ecall
-	la a0, newline
-	li a7, 4
-	ecall
 	addi a1, a1, 1
 	addi t2, t2, 4
 	j loopB
@@ -68,11 +57,6 @@ tela:	#preenche a tela de branco
 	addi s0, s0, 4
 	j tela
 
-sleep:
-	li a0, 1000
-	li a7, 32
-	ecall
-
 estado:
 	addi s0, s0, -1024
 	la t3, matrizA
@@ -85,15 +69,102 @@ estado:
 	addi t4, zero, 1
 	addi t6, zero, 0
 	addi t5, zero, 0
-	addi t0, zero, 256
-verifica:
+	addi t0, zero, 4
+	addi s10, zero, 1 #x
+	addi s9, zero, 1 #y
+	addi s8, zero, 16
+
+sleep:
+	li a0, 1000
+	li a7, 32
+	ecall
+		
+percorre:
 	beq t4, t6, pixel
-	beq t5, t0, fim
+	beq t5, a1, fim
+	beq s9, s8, soma
 	lw t6, 0(t3)
+	j verifica
+cont:
 	addi t3, t3, 4
 	addi t5, t5, 1
-	j verifica
+	addi s9, s9, 1
+	j percorre
 
+soma:
+	addi s10, s10, 1
+	addi s9, zero, 1
+	j cont
+	
+verifica:
+	la t2, matrizB
+	mul s7, t5, t0
+	add t2, t2, s7
+	sw t6, 0(t2)
+	addi s11, zero, 0
+	bgt s10, t4, verifica1
+	bgt s9, t4, verifica4
+	j verifica6
+	
+verifica1:
+	lw a0, -64(t3)
+	add s11, s11, a0
+	beq s9, t4, verifica3
+
+verifica2:
+	lw a0, -68(t3)
+	add s11, s11, a0
+	beq s9, s8, verifica4
+
+verifica3:
+	lw a0, -60(t3)
+	add s11, s11, a0
+	beq s9, t4, verifica6
+	
+verifica4:
+	lw a0, -4(t3)
+	add s11, s11, a0
+	beq s10, s8, verifica8
+
+verifica5:
+	lw a0, 60(t3)
+	add s11, s11, a0
+
+verifica6:
+	lw a0, 64(t3)
+	add s11, s11, a0
+	beq s9, s8, teste
+	
+verifica7:
+	lw a0, 68(t3)
+	add s11, s11, a0
+	
+verifica8:
+	lw a0, 4(t3)
+	add s11, s11, a0
+	j teste
+
+teste:	
+	addi a2, zero, 0
+	beq s11, a1, morte
+	addi a2, a2, 1
+	beq s11, a2, morte
+	addi a2, a2, 1
+	beq s11, a2, cont
+	addi a2, a2, 1
+	beq s11, a2, vida
+	bgt s11, a2, morte
+	
+morte:	
+	addi a2, zero, 0
+	sw a2, 0(t2)
+	j cont
+
+vida:	
+	addi a2, zero, 1
+	sw a2, 0(t2)
+	j cont
+	
 pixel:	
 	addi t5, t5, -1
 	lw t6, xmul
@@ -103,7 +174,7 @@ pixel:
 	sub s0, s0, t6
 	addi t5, t5, 1
 	addi t6, zero, 0
-	j verifica
+	j percorre
 fim:	
 	#la t2, matrizB
 	#lw a0, 460(t2)
